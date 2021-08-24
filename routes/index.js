c21dbc057110227ad9ec1161acf0efd68ab155c8
Lucas Almeida <lucasaclima03@gmail.com>
2021-08-24 09:10:29 -0300

    criação das rotas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,50 @@
+var conn = require('./../inc/db');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  conn.query(`
+    SELECT * FROM tb_menus ORDER BY title
+    `, (err, results)=>{
+      
+      if(err){
+        console.log(err);
+      }
+
+      res.render('index', { 
+        title: 'Restaurante Saboroso!',
+        menus: results 
+      
+      });
+
+    });
+  
+});
+
+router.get('/contacts', function(req,res,next){
+  res.render('contacts',{
+    title: 'Contato - Restaurante Saboroso!'
+  });
+});
+
+router.get('/menus', function(req,res,next){
+  res.render('menus',{
+    title: 'Menus - Restaurante Saboroso!'
+  });
+});
+
+router.get('/reservations', function(req,res,next){
+  res.render('reservations',{
+    title: 'Reservas - Restaurante Saboroso!'
+  });
+});
+
+router.get('/services', function(req,res,next){
+  res.render('services',{
+    title: 'Serviços - Restaurante Saboroso!'
+  });
 });
 
 module.exports = router;
